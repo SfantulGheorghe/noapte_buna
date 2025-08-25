9724e2e8c7fbc6bfd623450937a0d0c81078416a
SfantulGheorghe <81180635+SfantulGheorghe@users.noreply.github.com>
2025-08-25 02:33:50 +0200

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://sfantulgheorghe.github.io/noapte_buna/](https://sfantulgheorghe.github.io/noapte_buna/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/sfantulgheorghe/noapte_buna** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/sfantulgheorghe/noapte_buna** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,59 @@
+let one = 0
+basic.forever(function () {
+    one = 0
+    basic.showString("GO!")
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . # . #
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.clearScreen()
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . # . #
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.clearScreen()
+    one = randint(0, 5)
+    if (one == 1) {
+        for (let index = 0; index < 3 + 1; index++) {
+            basic.showLeds(`
+                . . # . .
+                # # . # .
+                . # # # .
+                . # . # .
+                . # . # .
+                `)
+        }
+        basic.showString("Anne")
+        basic.pause(100)
+        basic.clearScreen()
+        basic.showString("Noapte Buna, draga.")
+        basic.pause(100)
+        basic.clearScreen()
+        basic.showIcon(IconNames.Heart)
+        basic.pause(100)
+        basic.clearScreen()
+    } else {
+        basic.showIcon(IconNames.Skull)
+        basic.pause(100)
+        basic.clearScreen()
+        basic.showString("¶ Tu reviendras…")
+    }
+})
